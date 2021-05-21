6983d9b29c2fa7f315c1ea0f5c16c9e8c27741cf
webluxdesigns <webluxdesigns@gmail.com>
2021-05-21 14:46:11 -0700

    Add my answers for helloworld and variables

--- Exercise Files/Ch2/helloworld_start.py
+++ Exercise Files/Ch2/helloworld_start.py
@@ -1,3 +1,8 @@
 #
 # Example file for HelloWorld
 #
+
+def main()
+print("Hello World")
+name = input("What is your name?")
+print("Nice to meet you,", name)
--- Exercise Files/Ch2/variables_start.py
+++ Exercise Files/Ch2/variables_start.py
@@ -4,16 +4,29 @@
 
 # Declare a variable and initialize it
 
-
+carName = "Nissan Rouge"
+print(carName)
 
 # re-declaring the variable works
 
-
+carName = "Nissan 370z"
+print(carName)
 
 # ERROR: variables of different types cannot be combined
 
-
+x = 1
+y = "Dog"
+print(str(x) + y)
 
 # Global vs. local variables in functions
 
+favoriteName = "Kaliyah" #Global variable 
+
+def name():
+    global favoriteName #This will make it where the variable is in fact global so the variable outside the function will not print. 
+    favoriteName = "Jose" #Local variable 
+    print(favoriteName)
+
+name() #This is printing the local variable defined in the function
+print(favoriteName) #This is printing the global variable declared outside of the function
 
